c060356a8a8c7a49338eaf4582130593a98057f0
sapporojones <sapporojones@gmail.com>
2021-01-19 17:35:09 -0800

    initial commit - django list of lists

---
+++ to/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from . import views
+
+
+urlpatterns = [
+
+	path('', view.index, name='index'),
+
+    # path('', views.index, name='index'),
+    # path('results/', views.results, name='results'),
+    # path('about/', views.about, name='about'),
+    # path('post_search/', views.post_search, name='post_search'),
+
+]
